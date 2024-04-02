2d748b3ded1ef5d50b49945e5abcb04488649227
Aman <sikarwaraman26@gmail.com>
2024-04-02 09:38:24 +0530

    add home page

--- src/services/apis.tsx
+++ src/services/apis.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const appid = 'acfc0ccd3dac3d2fc462063689b77319'
 
 export const getCityFromCord = (lat:any, lon:any) =>{
-    return axios.get('http://api.openweathermap.org/geo/1.0/reverse', {params:{
+    return axios.get('https://api.openweathermap.org/geo/1.0/reverse', {params:{
         lat,
         lon,
         appid
@@ -19,7 +19,7 @@ export const getWeatherData = (lat:any, lon:any) =>{
 
 
 export const getLongLat = (q:any) =>{
-    return axios.get('http://api.openweathermap.org/geo/1.0/direct', {params:{
+    return axios.get('https://api.openweathermap.org/geo/1.0/direct', {params:{
         q,
         limit:5,
         appid
